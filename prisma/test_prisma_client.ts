b0843443f1e9c39378a187a51d8558e13677134b
dog <1@1.com>
2025-06-02 21:42:06 +0800

    prisma graphQL, pagenation

--- prisma/test_prisma_client.ts
+++ prisma/test_prisma_client.ts
@@ -1,16 +1,29 @@
 import {PrismaClient} from "@prisma/client"
 
 const client = new PrismaClient()
+// async function main() {
+//     let result = await client.user.create({
+//         data: {
+//             name: 'Alice',
+//             email: '1@1.com',
+//             posts: {
+//                 create: {
+//                     title:'second',
+//                     content: '2',
+//                 }
+//             }
+//         }
+//     })
+//     console.log("result:", result)
+// }
+
 async function main() {
-    let result = await client.user.create({
-        data: {
-            name: 'Alice',
-            email: '1@1.com',
-            posts: {
-                create: {
-                    title:'second',
-                    content: '2',
-                }
+    let result = await client.user.findMany({
+        skip: 5, // 分页，从第6条查起
+        take: 2, // 分页，查2条
+        where: {  // GraphQL筛选
+            name: {
+                startsWith: 'A'
             }
         }
     })
